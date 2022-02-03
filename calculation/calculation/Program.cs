76c5c72a4db61706a3efaeef66310ee90f21f0b2
lk <lakshmikumari14479@gmail.com>
2022-02-03 17:17:38 +0530

    first

---
+++ calculation/calculation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculation
+{
+    class Program
+    {
+        public void calculation()
+        {
+            Console.WriteLine("enter a principle of amount");
+            double principle = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter a time");
+            double time = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter a rate of interest");
+            double rate = Convert.ToDouble(Console.ReadLine());
+            double simpleinterest = principle * time * rate / 100;
+            Console.WriteLine(simpleinterest);
+        }
+        static void Main(string[] args)
+        {
+            Program objectname = new Program();
+            objectname.calculation();
+            Console.Read();  
+            }
+        }
+    }
+
